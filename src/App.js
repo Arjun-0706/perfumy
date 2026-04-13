c9df7d64b4827e0d6211b4122938540c22539b29
arjunkanthan912@gamil.com <arjunkanthan912@gmail.com>
2026-04-13 18:45:30 +0530

    updated project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Navbar from "./components/Navbar"
+import Search from "./components/Searchbar"
+import Product from "./components/product"
+import Footer from "./components/Footer"
+import About from "./components/About"
+
+function App(){
+return(
+    <div>
+    <Navbar></Navbar>
+    <Search></Search>
+    <Product></Product>
+    <About></About>
+    <Footer></Footer>
+  </div>
+  )
+}
+export default App
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <nav>
+      <h1>Perfumy</h1>
+
+      <ul>
+        <li><a>Home |</a></li>
+        <li><a>Product |</a></li>
+        <li><a>Contact |</a></li>
+        <li>About |</li>
+      </ul>
+    </nav>
+  )
+}
+export default Navbar
